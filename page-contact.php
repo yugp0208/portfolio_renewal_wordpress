5620cf6a623fe3410e55cdb49814532751fd33b9
yugp0208 <yuu.fepnhdper2000@gmail.com>
2024-03-28 02:39:54 +0900

    お問い合わせフォームの調整

--- page-contact.php
+++ page-contact.php
@@ -8,6 +8,9 @@
   <section class="c-section p-contact">
       <div class="c-section__inner p-contact__inner">
         <h2 class="p-contact__ttl js-pageTtl">Contact</h2>
+        <p class="p-contact__ttl__desc">制作の依頼・ご相談などお気軽にお問い合わせください。<br>
+        下記フォームよりわかる範囲でご記入ください。必須の項目は必ずご記入お願いします。</p>
+        <?php echo apply_filters('the_content', '[contact-form-7 id="2019ae2" title="Contact - お問い合わせフォーム"]'); ?>
       </div>
   </section>
 </main>
